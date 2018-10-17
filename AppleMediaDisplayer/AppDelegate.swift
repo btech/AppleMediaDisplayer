35df46163cb3ca2acda7ca60df35061077f857d5
me <b@classmate.one>
2018-10-17 08:18:56 -0400

    Initial commit

---
+++ AppleMediaDisplayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AppleMediaDisplayer
+//
+//  Created by me on 10/16/18.
+//  Copyright © 2018 me. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+    func applicationWillTerminate(_ application: UIApplication) {}
+}
