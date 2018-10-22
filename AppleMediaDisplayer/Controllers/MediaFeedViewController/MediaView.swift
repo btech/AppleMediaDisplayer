e206bde06227f2acaad22df941adadf2406c7dbf
me <b@classmate.one>
2018-10-22 01:42:00 -0400

    Add `MediaView`
    Displays the content of a `Media` object

--- AppleMediaDisplayer/Controllers/MediaFeedViewController/MediaView.swift
+++ AppleMediaDisplayer/Controllers/MediaFeedViewController/MediaView.swift
@@ -9,6 +9,81 @@
 import Foundation
 import UIKit
 
+
+class MediaView: UIView {
+    
+    private var media: Media?
+    
+    
+    private let imageView: URLImageView = {
+        
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.contentMode = .scaleAspectFit
+        $0.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        $0.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        return $0
+        
+    }(URLImageView(frame: .zero))
+    
+    private let titleLabel: UILabel = {
+        
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
+        
+        return $0
+        
+    }(UILabel(frame: .zero))
+    
+    private let creatorLabel: UILabel = {
+        
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
+        
+        return $0
+        
+    }(UILabel(frame: .zero))
+    
+    
+    func bind(with media: Media) {
+        
+        self.media = media
+        
+        imageView.loadImage(from: media.imageURL)
+        titleLabel.text = media.title
+        creatorLabel.text = media.creatorName
+    }
+    
+    
+    init() {
+        super.init(frame: .zero)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubviews([imageView, titleLabel, creatorLabel])
+        
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
+        titleLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: creatorLabel.topAnchor).isActive = true
+        
+        creatorLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor).isActive = true
+        creatorLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        creatorLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
+        creatorLabel.heightAnchor.constraint(equalTo: titleLabel.heightAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 fileprivate class URLImageView: UIImageView {
     
     private var url: URL?
